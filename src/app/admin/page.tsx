367b378a58e26846f67f5f1ed8a3d0e178299b4e
Talon Carballeira <taloncarb@gmail.com>
2025-01-23 14:06:33 -0700

    Messed with admin look

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -26,11 +26,10 @@ export default function AdminDash() {
   
 
   return (
-    <main className="min-h-screen bg-white p-4 ">
+    <main className="min-h-screen bg-cc-gold-faint p-4 ">
       {/* Header */}
       <header className="mb-6 text-center">
-        <h1 className="text-4xl font-bold text-gray-800">Student AI Use Information</h1>
-        <p className="text-lg text-gray-600">Subtitle here?</p>
+        <h1 className="text-4xl font-bold text-gray-800 border-b-8 border-double border-cc-gold">Student AI Use Information</h1>
       </header>
 
       {/* Content */}
@@ -40,7 +39,7 @@ export default function AdminDash() {
         <div className="flex-1 grid grid-cols-2 gap-4">
             {/*Graph1*/}
             <div
-              className="bg-white border rounded p-4 shadow-lg h-60 ml-10 mb-10"
+              className="border border-4 border-double border-cc-gold rounded p-4 shadow-lg h-60 ml-10 mb-10"
             >
               <h2 className="font-bold text-xl mb-2 text-cc-charcoal">{graph1.title}</h2>
               <p className='text-gray-600'>{graph1.content}</p>
@@ -48,7 +47,7 @@ export default function AdminDash() {
             </div>
             {/*Graph2*/}
             <div
-              className="bg-white border rounded p-4 shadow-lg h-60 ml-10 "
+              className="border border-4 border-double border-cc-gold rounded p-4 shadow-lg h-60 ml-10 "
             >
               <h2 className="font-bold text-xl mb-2 text-cc-charcoal">{graph2.title}</h2>
               <p className='text-gray-600'>{graph2.content}</p>
@@ -56,7 +55,7 @@ export default function AdminDash() {
               </div>
               {/*Graph3*/}
             <div
-              className="bg-white border rounded p-4 shadow-lg h-60 ml-10"
+              className="border border-4 border-double border-cc-gold rounded p-4 shadow-lg h-60 ml-10"
             >
               <h2 className="font-bold text-xl mb-2 text-cc-charcoal">{graph3.title}</h2>
               <p className='text-gray-600'>{graph3.content}</p>
@@ -64,7 +63,7 @@ export default function AdminDash() {
             </div>
             {/*Graph4*/}
             <div
-              className="bg-white border rounded p-4 shadow-lg h-60 ml-10 "
+              className="border border-4 border-double border-cc-gold rounded p-4 shadow-lg h-60 ml-10 "
             >
               <h2 className="font-bold text-xl mb-2 text-cc-charcoal">{graph4.title}</h2>
               <p className='text-gray-600'>{graph4.content}</p>
@@ -77,7 +76,7 @@ export default function AdminDash() {
 
         {/* Right Side: Data Box */}
         <div className="w-1/2 ml-4 mr-10 ml-10 ">
-          <div className="bg-white border rounded p-4 shadow-lg min-h-full">
+          <div className="border border-4 border-double border-cc-gold rounded p-4 shadow-lg min-h-full">
             <h2 className="font-bold text-xl mb-2 text-cc-charcoal">{queryBox.title}</h2>
             <p className='text-gray-600'>{queryBox.content}</p>
           </div>
