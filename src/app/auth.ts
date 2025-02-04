dcb8cfd2e2ccefc33dd0230f3fc456f3a4c0054e
Talon Carballeira <taloncarb@gmail.com>
2025-02-04 10:58:27 -0700

    messing around with ideas

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,5 +1,20 @@
 import NextAuth from "next-auth";
  
+export function firstGetRequest(){
+  const CLIENT_ID = process.env.CANVAS_CLIENT_ID
+  const SECRET_ID = process.env.CANVAS_CLIENT_SECRET
+  const STATE = "RANDOMSTATE"
+  const REDIRECT = encodeURIComponent("http://localhost:3000/api/auth/callback/canvas")
+  const url = `https://coloradocollege.instructure.com/login/oauth2/auth?client_id=${CLIENT_ID}&response_type=code&state=${STATE}&redirect_uri=http://localhost:3000/api/auth/callback/canvas`
+  fetch(url, {
+    method: 'GET',
+    redirect: 'follow'
+  }).then(response => console.log(response)
+
+  ).catch(function(err) {
+    console.info(err + " url: " + url);
+});
+}
 
 export const { handlers, auth, signIn } = NextAuth({
   providers: [
@@ -10,8 +25,7 @@ export const { handlers, auth, signIn } = NextAuth({
       authorization: {
         url: "https://coloradocollege.instructure.com/login/oauth2/auth",
         params: {
-          response_type: "code",
-          
+          response_type: "code", // canvas docs say this needs to be code
           scope: "url:GET|/api/v1/courses" // canvas docs say this is important?
         }
       },
@@ -23,12 +37,12 @@ export const { handlers, auth, signIn } = NextAuth({
       },
       clientId: process.env.CANVAS_CLIENT_ID,
       clientSecret: process.env.CANVAS_CLIENT_SECRET,
-      async profile(profile) { // may get problems here, should change to what canvas has. 
+      async profile(profile) { // Should be how you access info? Maybe change userInfo url to …/self/enrollments to get course
         return {
           id: profile.login_id,
           name: profile.name,
         };
-      }
+      },  
     }
   ]
 });
