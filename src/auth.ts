963e116e4c5cd10cc88b20826bf73f816b29ada7
Skingr <s_king@coloradocollege.edu>
2025-02-08 22:33:15 -0700

    fix: client id

--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,15 @@
-
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
 import { createUser, getUserByEmail } from "./db/queries";
 import { Email } from "./sharedTypes/types";
 import useUserStore from "./hooks/store/useUser";
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
   callbacks: {
     async signIn({ user }) {
       // restrict access tojust @coloradocollege.edu emails
@@ -18,5 +22,4 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   pages: {
     signIn: "/auth/login",
   },
-})
-
+});
